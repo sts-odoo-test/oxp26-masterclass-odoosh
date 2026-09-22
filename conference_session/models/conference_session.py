51bd8a6483ccd163196d83b7aff7165d9bd96f42
Your Name <you@example.com>
2026-09-22 22:48:04 +0000

    [FIX] an hour is 60min

--- conference_session/models/conference_session.py
+++ conference_session/models/conference_session.py
@@ -21,4 +21,4 @@ class ConferenceSession(models.Model):
     @api.depends('duration')
     def _compute_duration_in_hours(self):
         for session in self:
-            session.duration_in_hours = session.duration / 100.0
+            session.duration_in_hours = session.duration / 60.0
